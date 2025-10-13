cf3fb8a983ffc28428286740c561b8c5e9a4222a
fernandolim41 <pdvmobilidade@gmail.com>
2025-10-13 22:28:55 +0000

    refactor: Organizar e otimizar o código do componente Chart

--- src/components/ui/chart.jsx
+++ src/components/ui/chart.jsx
@@ -67,13 +67,13 @@ const ChartStyle = ({
           .map(([theme, prefix]) => `
 ${prefix} [data-chart=${id}] {
 ${colorConfig
-.map(([key, itemConfig]) => {
-const color =
-  itemConfig.theme?.[theme] ||
-  itemConfig.color
-return color ? `  --color-${key}: ${color};` : null
-})
-.join("\n")}
+              .map(([key, itemConfig]) => {
+                const color =
+                  itemConfig.theme?.[theme] ||
+                  itemConfig.color
+                return color ? `  --color-${key}: ${color};` : null
+              })
+              .join("\n")}
 }
 `)
           .join("\n"),
@@ -197,9 +197,13 @@ function ChartTooltipContent({
                         {itemConfig?.label || item.name}
                       </span>
                     </div>
-                    {item.value && (
+                    {typeof item.value === "number" ? (
                       <span className="text-foreground font-mono font-medium tabular-nums">
                         {item.value.toLocaleString()}
+                      </span>
+                    ) : (
+                      <span className="text-foreground font-mono font-medium tabular-nums">
+                        {item.value ?? ""}
                       </span>
                     )}
                   </div>
@@ -274,8 +278,8 @@ function getPayloadConfigFromPayload(
 
   const payloadPayload =
     "payload" in payload &&
-    typeof payload.payload === "object" &&
-    payload.payload !== null
+      typeof payload.payload === "object" &&
+      payload.payload !== null
       ? payload.payload
       : undefined
 
